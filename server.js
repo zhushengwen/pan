f51c56fd49e3b077386207c2ce9846ca6dcd3f8d
zhushegnwen <zhushegnwenzsw@126.com>
2017-06-15 07:03:16 +0000

    fix

--- server.js
+++ server.js
@@ -67,3 +67,15 @@ server = http.createServer(function (req, res) {
 
 console.log('listening on port ' + PROXY_PORT);
 server.listen(PROXY_PORT);
+
+
+/*
+var http = require('http');
+var setup = require('proxy');
+ 
+var server = setup(http.createServer());
+server.listen(process.env.PORT, function () {
+  var port = server.address().port;
+  console.log('HTTP(s) proxy server listening on port %d', port);
+});
+*/
